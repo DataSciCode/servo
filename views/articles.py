8f9dd694cd4ec4a36ee2f9950b65957af0e0680c
Filipp Lepalaan <filipp@mac.com>
2012-05-24 07:36:29 +0300

    better

---
+++ views/articles.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from bson.objectid import ObjectId
+from servo3.models import Article, Tag
+
+def index(req):
+	articles = Article.objects.all()
+	return render(req, "articles/index.html", {"data": articles})
+
+def remove(req, id):
+	pass
+
+def edit(req, id):
+	pass
+
+def save(req):
+	pass
+
+def view(req, id):
+	pass
---
+++ views/specs.py
@@ -0,0 +1,16 @@
+from servo3.models import Spec
+
+def index(req):
+	pass
+
+def create(req):
+	pass
+
+def save(req):
+	pass
+
+def remove(req, id = None):
+	pass
+
+def edit(req, id):
+	pass
