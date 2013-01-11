810b0dcbe4e865e2803500e735f76ff19cad00c2
Filipp Lepalaan <f@0x00.co>
2013-01-11 07:57:59 +0200

    Clean up device editing

--- servo/urls/device.py
+++ servo/urls/device.py
@@ -2,12 +2,12 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('servo.views.device',
     url(r'^$', 'index'),
+    url(r'^new/$', 'edit'),
     url(r'^search$', 'search'),
     url(r'^search/(?P<query>\w+)$', 'index'),
     url(r'^tag/(?P<tag>\d+)/$', 'index'),
     url(r'^(\d+)/edit/$', 'edit'),
     url(r'^(\d+)/view/$', 'view'),
-    url(r'^new/$', 'create'),
     url(r'^remove/$', 'remove'),
     url(r'^(\d+)/remove/$', 'remove'),
     url(r'^(\d+)?/save/$', 'save'),
